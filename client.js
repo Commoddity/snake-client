28debd47b14d7f649428710183dadef100b23132
Pascal <pascalvanleeuwen604@gmail.com>
2019-12-04 21:20:35 +0000

    Added name function

--- client.js
+++ client.js
@@ -6,15 +6,19 @@ const connect = function() {
     host: '192.168.88.149',
     port: 50541
   });
+
   conn.setEncoding('utf8');
+
+  conn.on('connect', () => {
+    console.log("Connected to SERVER!");
+    conn.write("Name: PVL");
+  });
   
+  conn.on('data', data => {
+    console.log(data);
+  });
+
   return conn;
 };
 
-const conn = connect();
-
-conn.on('data', data => {
-  console.log(data);
-});
-
-module.exports = connect;
+module.exports = { connect };
